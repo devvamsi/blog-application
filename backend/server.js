fca77da7caa2b5c462fea2bc4cc27ddbcd28519a
pallavi <your-email@gmail.com>
2026-08-20 20:15:12 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,5 @@ app.use("/api/blog", require("./routes/blogRoutes"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
